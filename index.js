53da0d850357eb6b6de057e698e095a89ddf1ec2
karsyn ansari <karsyn.ansari@gmail.com>
2022-01-18 15:53:34 -0700

    commit tree fab988523b57c1ecd28cc0ef5634c24fc5bead54

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
+// const dodger = document.getElementById("dodger");
+// function moveDodgerLeft() {
+//   const leftNumbers = dodger.style.left.replace("px", "");
+//   const left = parseInt(leftNumbers, 10);
+
+//   if (left > 0) {
+//     dodger.style.left = `${left - 1}px`;
+//   }
+// }
+// document.addEventListener("keydown", function (e) {
+//   if (e.key === "ArrowLeft") {
+//     moveDodgerLeft();
+//   }
+// });
+
+// function moveDodgerRight() {
+//   const leftNumbers = dodger.style.left.replace("px", "");
+//   const left = parseInt(leftNumbers, 10);
+
+//   if (left > 0) {
+//     dodger.style.left = `${left + 1}px`;
+//   }
+// }
+// document.addEventListener("keydown", function (e) {
+//   if (e.key === "ArrowRight") {
+//     moveDodgerRight();
+//   }
+// });
+
 const dodger = document.getElementById("dodger");
+
 function moveDodgerLeft() {
   const leftNumbers = dodger.style.left.replace("px", "");
   const left = parseInt(leftNumbers, 10);
@@ -7,20 +37,31 @@ function moveDodgerLeft() {
     dodger.style.left = `${left - 1}px`;
   }
 }
-document.addEventListener("keydown", function (e) {
-  if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
-  }
-});
+//
 function moveDodgerRight() {
-  const leftNumbers = dodger.style.left.replace("px", "");
-  const left = parseInt(leftNumbers, 10);
+  const rightNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(rightNumbers, 10);
+
   if (left < 360) {
     dodger.style.left = `${left + 1}px`;
   }
 }
+//// function moveDodgerRight() {
+//   const leftNumbers = dodger.style.left.replace("px", "");
+//   const left = parseInt(leftNumbers, 10);
+
+//   if (left > 0) {
+//     dodger.style.left = `${left + 1}px`;
+//   }
 document.addEventListener("keydown", function (e) {
-  if (e.key === "ArrowRight") {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
     moveDodgerRight();
+    // console.log("this is working");
   }
 });
+
+//Add anotion condition to the callback function to call moveDodgerRight(else??)
+//inside the moveDodgerRight function, move the dodger left + 1px
+//write an if statement inside of moveDodgerRight that prevents the dodger from disappearing
